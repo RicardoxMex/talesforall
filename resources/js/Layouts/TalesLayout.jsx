82d9258fd7c524c2687d9aa5a2b1367911a7e802
Ricardo Mex <ricardo.mexan@hotmail.com>
2024-08-05 11:29:51 -0600

    se cargaron los logos

--- resources/js/Layouts/TalesLayout.jsx
+++ resources/js/Layouts/TalesLayout.jsx
@@ -5,16 +5,21 @@ import NavLink from '@/Components/NavLink';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
 import { Head, Link } from '@inertiajs/react';
 
-export default function TalesLayout({ auth, header, children, title, style='' }) {
+export default function TalesLayout({ auth, header, children, title, style = '' }) {
     const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
 
     return (
-        <div className={ `h-screen flex flex-col bg-gray-100`}>
+        <div className={`h-screen flex flex-col bg-gray-100`}>
             <Head title={title} />
             <nav className=" sticky top-0 z-10 bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="flex justify-between h-16">
                         <div className="flex">
+                            <div className="shrink-0 flex items-center">
+                                <Link href="/">
+                                    <ApplicationLogo className="block h-12 w-auto fill-current rounded-full" />
+                                </Link>
+                            </div>
                             <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                                 <NavLink href={route('create-story')} active={route().current('create-story')}>
                                     Crear Historia
@@ -161,7 +166,7 @@ export default function TalesLayout({ auth, header, children, title, style='' })
             <div className="md:py-12 bg-gray-100 min-h-screen">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className={`bg-white shadow-md rounded-lg p-1 md:p-6 ${style}`}>
-                    <h1 className="m-0 text-3xl font-bold text-gray-900 mb-6">{title}</h1>
+                        <h1 className="m-0 text-3xl font-bold text-gray-900 mb-6">{title}</h1>
                         <main className='flex-1 overflow-y-auto'>{children}</main>
                     </div>
                 </div>
